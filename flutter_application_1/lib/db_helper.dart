62f1ddb7fc5193223e3ad39cc3c22ecb092fe254
Eliseo <sanzeliseo@yahoo.com.ar>
2024-03-20 16:44:26 -0300

    fixed

--- flutter_application_1/lib/db_helper.dart
+++ flutter_application_1/lib/db_helper.dart
@@ -7,46 +7,64 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 class DbHelper {
   late Database _database;
 
+  // Constructor asíncrono
   DbHelper() {
     _initializeDatabase();
   }
 
   Future<void> _initializeDatabase() async {
-    databaseFactory = databaseFactoryFfi;
-    _database = await _openDatabase();
+    try {
+      _database = await _openDatabase();
+    } catch (e) {
+      // Manejar la excepción, por ejemplo, imprimir un mensaje de error
+      print('Error al abrir la base de datos: $e');
+    }
   }
 
   Future<Database> get database async {
-    // Verificar si la base de datos está abierta y devolverla
-    if (_database.isOpen) {
-      return _database;
-    }
-    // Si no está abierta, abrir la base de datos y devolverla
-    _database = await _openDatabase();
+    // Esperar a que la base de datos esté inicializada
+    await _initializeDatabase();
     return _database;
   }
 
   Future<Database> _openDatabase() async {
-    return openDatabase(join(await getDatabasesPath(), 'ecosistema.db'),
+    try {
+      // Abrir la base de datos y crear la tabla si no existe
+      return await openDatabase(
+        join(await getDatabasesPath(), 'ecosistema.db'),
         onCreate: (db, version) {
-      return db.execute(
-          "CREATE TABLE usuarios (id INTEGER PRIMARY KEY, nombre TEXT, edad INT)");
-    }, version: 1);
+          return db.execute(
+            "CREATE TABLE usuarios (id INTEGER PRIMARY KEY, nombre TEXT, edad INT)",
+          );
+        },
+        version: 1,
+      );
+    } catch (e) {
+      // Capturar cualquier excepción y mostrar el mensaje de error
+      print('Error al abrir la base de datos: $e');
+      rethrow; // Reenviar la excepción para que sea manejada en un nivel superior si es necesario
+    }
   }
 
   Future<List<Usuario>> getUsuariosQry() async {
+    // Obtener la base de datos
     Database db = await database;
+    // Realizar la consulta y mapear los resultados
     final List<Map<String, dynamic>> usuariosMap = await db.query("usuarios");
     return List.generate(
-        usuariosMap.length,
-        (i) => Usuario(
-            edad: usuariosMap[i]['edad'],
-            id: usuariosMap[i]["id"],
-            nombre: usuariosMap[i]["nombre"]));
+      usuariosMap.length,
+      (i) => Usuario(
+        edad: usuariosMap[i]['edad'],
+        id: usuariosMap[i]["id"],
+        nombre: usuariosMap[i]["nombre"],
+      ),
+    );
   }
 
   Future<void> agregarUsuario(String nombre, int edad) async {
+    // Obtener la base de datos
     final db = await database;
+    // Insertar el usuario en la base de datos
     await db.insert(
       'usuarios',
       {'nombre': nombre, 'edad': edad},
@@ -55,13 +73,17 @@ class DbHelper {
   }
 
   Future<void> mostrar() async {
+    // Obtener la base de datos
     final db = await database;
+    // Realizar la consulta y mostrar los resultados
     final data = await db.query('usuarios');
     print(data);
   }
 
   Future<void> borrarTodo() async {
+    // Obtener la base de datos
     final db = await database;
+    // Borrar todos los usuarios de la base de datos
     await db.delete('usuarios');
   }
 }
